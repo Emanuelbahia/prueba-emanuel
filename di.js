f63270158002edef8437453f59e751032765366c
diego vallejo <vallejodiego@live.com>
2022-04-12 15:57:43 -0300

    volvi a cambiar el di, que te aparece?

--- di.js
+++ di.js
@@ -1,14 +1,4 @@
-<<<<<<< HEAD
-console.log("que tal ? ");
-
-let saludo = "hola";
-let comida = "asado";
-let deporte = "tenis";
-
-console.log(saludo);
-=======
 let pregunta = "¿que tal?"
 function saludo(){return pregunta}
 
 console.log(saludo())
->>>>>>> 7461c1a80b8dccc30721370774f86e7421140b50
